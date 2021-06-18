d1d40a07770799ed0915d3c3d9ff34126be4c052
Christopher Mark Gore <cgore@cgore.com>
2021-06-18 11:43:19 -0500

    splitting the URL string

--- src/bin/ethereum-gas-price.rs
+++ src/bin/ethereum-gas-price.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://api.zapper.fi/v1/gas-price")
+    let resp = reqwest::get("http://api.zapper.fi/v1".to_owned() + "/gas-price")
         .await?;
     println!("{:#?}", resp);
     Ok(())
