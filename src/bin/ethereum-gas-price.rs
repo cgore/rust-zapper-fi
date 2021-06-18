0dc2aea9cfc9467e90cd862f5b6e20520785ae9c
Christopher Mark Gore <cgore@cgore.com>
2021-06-18 14:22:23 -0500

    Sending the API key so we don't get a 401

--- src/bin/ethereum-gas-price.rs
+++ src/bin/ethereum-gas-price.rs
@@ -2,7 +2,10 @@ use zapper_fi::rest_api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get(rest_api::API.to_owned() + "/gas-price")
+    let client = reqwest::Client::new();
+    let resp = client.get(rest_api::API.to_owned() + "/gas-price")
+        .query(&[("api_key",rest_api::API_KEY)])
+        .send()
         .await?;
     println!("{:#?}", resp);
     Ok(())
