7b076f0c4e58f8a11f57120877f0b7575db69107
Christopher Mark Gore <cgore@cgore.com>
2021-06-18 15:11:04 -0500

    Get the standard gas price out of the json payload

--- src/bin/ethereum-gas-price.rs
+++ src/bin/ethereum-gas-price.rs
@@ -1,12 +1,15 @@
+use serde_json;
 use zapper_fi::rest_api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
-    let resp = client.get(rest_api::API.to_owned() + "/gas-price")
+    let resp: serde_json::Value = client.get(rest_api::API.to_owned() + "/gas-price")
         .query(&[("api_key",rest_api::API_KEY)])
         .send()
+        .await?
+        .json()
         .await?;
-    println!("{:#?}", resp);
+    println!("{:#?}", resp["standard"]);
     Ok(())
 }
