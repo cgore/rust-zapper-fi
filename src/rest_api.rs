1318662009dffc9f10fb8c04f3cd41b3c52f4f02
Christopher Mark Gore <cgore@cgore.com>
2021-06-18 11:12:25 -0500

    Adding VALID_FARMS, VALID_POOLS, and VALID_NETWORKS

--- src/rest_api.rs
+++ src/rest_api.rs
@@ -1,6 +1,9 @@
 static API: &str = "http://api.zapper.fi/v1";
+
 static API_CACHE_TIMEOUT_SECONDS: i32 = 60;
+
 static API_KEY: &str = "96e0cc51-a62e-42ca-acee-910ea7d2a241";
+
 static VALID_PROTOCOLS: &'static [&str] = &[
     "aave", "aave-amm", "aave-v2", "alpha", "b-protocol", "badger", "balancer", "bancor",
     "barnbridge", "bitcoin", "compound", "cover", "cream", "curve", "defisaver",
@@ -9,4 +12,18 @@ static VALID_PROTOCOLS: &'static [&str] = &[
     "1inch", "pancakeswap", "nft", "other", "pickle", "pooltogether", "quickswap",
     "rari", "realt", "reflexer", "saddle", "sfinance", "shell", "smoothy", "snowswap",
     "sushiswap", "swerve", "synthetix", "tokensets", "tokens", "uniswap", "uniswap-v2",
-    "unit", "value", "vesper", "xsigma", "yearn"];
+    "unit", "value", "vesper", "xsigma", "yearn"
+];
+
+static VALID_FARMS: &'static [&str] = &[
+    "masterchef", "single-staking", "geyser", "gauge"
+];
+
+static VALID_POOLS: &'static [&str] = &[
+    "balancer", "bancor", "curve", "loopring", "oneInch", "pancakeswap", "quickswap",
+    "sfinance", "snowswap", "sushiswap", "uniswap", "linkswap", "dodo", "saddle", "xsigma"
+];
+
+static VALID_NETWORKS: &static [&str] = &[
+    "ethereum", "binance-smart-chain", "polygon", "xdai"
+];
