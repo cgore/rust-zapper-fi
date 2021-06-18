d206190f4fd5be9fb061fd6e3c3aef3616d8671b
Christopher Mark Gore <cgore@cgore.com>
2021-06-18 11:58:24 -0500

    Almost there?

--- src/bin/ethereum-gas-price.rs
+++ src/bin/ethereum-gas-price.rs
@@ -1,6 +1,8 @@
+use zapper_fi::rest_api;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://api.zapper.fi/v1".to_owned() + "/gas-price")
+    let resp = reqwest::get(rest_api::API + "/gas-price")
         .await?;
     println!("{:#?}", resp);
     Ok(())
