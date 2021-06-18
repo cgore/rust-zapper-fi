a28604acce4cd105e9d822e0683ff6ee7b82ce9e
Christopher Mark Gore <cgore@cgore.com>
2021-06-18 11:08:15 -0500

    Adding some basic statics

---
+++ src/rest_api.rs
@@ -0,0 +1,12 @@
+static API: &str = "http://api.zapper.fi/v1";
+static API_CACHE_TIMEOUT_SECONDS: i32 = 60;
+static API_KEY: &str = "96e0cc51-a62e-42ca-acee-910ea7d2a241";
+static VALID_PROTOCOLS: &'static [&str] = &[
+    "aave", "aave-amm", "aave-v2", "alpha", "b-protocol", "badger", "balancer", "bancor",
+    "barnbridge", "bitcoin", "compound", "cover", "cream", "curve", "defisaver",
+    "derivadex", "dhedge", "dforce", "dodo", "dsd", "dydx", "esd", "futureswap", "idle",
+    "harvest", "hegic", "keeper-dao", "linkswap", "loopring", "maker", "mooniswap",
+    "1inch", "pancakeswap", "nft", "other", "pickle", "pooltogether", "quickswap",
+    "rari", "realt", "reflexer", "saddle", "sfinance", "shell", "smoothy", "snowswap",
+    "sushiswap", "swerve", "synthetix", "tokensets", "tokens", "uniswap", "uniswap-v2",
+    "unit", "value", "vesper", "xsigma", "yearn"];
